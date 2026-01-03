9ead23e7db8ce6d40a853e1805a525a01d1c4028
Tim Schopinski <91907758+timschopinski@users.noreply.github.com>
2026-01-03 13:48:19 +0100

    add frontend router

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,58 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../stores/auth'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Dashboard from '../views/Dashboard.vue'
+import RoomDetail from '../views/RoomDetail.vue'
+import MyReservations from '../views/MyReservations.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/room/:id',
+      name: 'room-detail',
+      component: RoomDetail
+    },
+    {
+      path: '/my-reservations',
+      name: 'my-reservations',
+      component: MyReservations,
+      meta: { requiresAuth: true }
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.requiresAuth && !authStore.token) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
