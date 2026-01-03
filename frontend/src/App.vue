da2530ad69f34764208467ee52433cc763807653
Tim Schopinski <91907758+timschopinski@users.noreply.github.com>
2026-01-03 13:45:34 +0100

    init frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Navigation from './components/Navigation.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-secondary-50">
+    <Navigation />
+    <RouterView />
+  </div>
+</template>
